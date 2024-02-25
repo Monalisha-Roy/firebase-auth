cade1ec3e50ff643aeb67ee247cecd0ed96c6204
Monalisha Roy <118293636+Monalisha-Roy@users.noreply.github.com>
2024-02-25 10:58:18 +0530

    Add files via upload

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import Login from "@/component/login";
+import Image from "next/image";
+
+export default function Logincomp() {
+    return (
+        <main className="bg-white flex items-center justify-center h-screen">
+            <Login/>
+        </main>
+    )
+}
